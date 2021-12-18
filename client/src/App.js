2c13d5b7166fa43d9d5b158ac42627c62c68496a
jjcatulle <38570952+jjcatulle@users.noreply.github.com>
2021-12-17 23:36:51 -0500

    lit

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,43 @@
 import { GlobalStyle } from "./global.styles";
 import { Routes, Route, Link, Router } from "react-router-dom";
 import React, { lazy, Suspense } from "react";
-const LandingPage = lazy(() => import("./pages/HomePage/HomePage.page"));
+
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+
+const LandingPage = lazy(
+  () =>
+    new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(import("./pages/HomePage/HomePage.page"));
+      }, 5000);
+    })
+);
 const DashboardPage = lazy(() => import("./pages/Dashboard/Dashboard.page"));
 
+function CircularIndeterminate() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <div
+        style={{
+          width: "100vw",
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        <CircularProgress style={{ color: "#b22c00", margin: "auto" }} />
+      </div>
+    </Box>
+  );
+}
 function App() {
   return (
     <div>
       <GlobalStyle />
 
-      <Suspense fallback={<h1>Still Loading…</h1>}>
+      <Suspense fallback={<CircularIndeterminate />}>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
